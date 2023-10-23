bac9528145e2ea43cc73c784e8c3f00b213eb89d
anastasijamilovic <anastasija.milovic02@gmail.com>
2023-10-23 20:45:25 +0200

    React

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Class16 from "./components/16.cas/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Class16></Class16>
+  </React.StrictMode>
+);
