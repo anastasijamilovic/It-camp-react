cf1f4875fdc873bc1baba9c367d57999e96b66f5
anastasijamilovic <anastasija.milovic02@gmail.com>
2023-11-13 20:55:40 +0100

    React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import Class16 from "./components/16.cas/index.jsx";
-import Class20 from "./components/21.cas";
-
+// import Class20 from "./components/21.cas";
+// import Class24 from "./components/24.cas";
 // import ToDoList from "./components/16.cas/ToDoList";
 // import ProductCard from "./components/20.cas/ProductCard";
+import Fetch from "./components/25.cas/fetch";
+import Class25 from "./components/25.cas";
 
 function App() {
   // return <h1>tsfvgh</h1>;
   // <ToDoList></ToDoList>;
-  return <Class20 />;
+  // return <Class20 />;
+  return <Class25 />;
 }
 
 export default App;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,15 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // import Class16 from "./components/16.cas/index.jsx";
-import Class20 from "./components/21.cas/index.jsx";
+// import Class20 from "./components/21.cas/index.jsx";
+import Class25 from "./components/25.cas/index.jsx";
 
-import ProductCard from "./components/21.cas/ProductCards.jsx";
+// import ProductCard from "./components/21.cas/ProductCards.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//     {/* <Class16></Class16> */}
+//     {/* <Class20></Class20> */}
+//     {/* <Class20 /> */}
+//     <Class24 />
+//   </React.StrictMode>
+// );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    {/* <Class16></Class16> */}
-    {/* <Class20></Class20> */}
-    <Class20 />
+    {/* <Class24 /> */}
+    <Class25 />
   </React.StrictMode>
 );
